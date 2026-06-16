4278bac3fc9d2f2634b515aeba0a548dba9c1847
AveDiscordia <guinto.eris0618@gmail.com>
2026-06-16 09:58:38 -0400

    htmlnode string formatting less complicated

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,53 +22,9 @@ class HTMLNode:
         for attribute, value in self.props.items():
             props_as_html += f' {attribute}="{value}"'
         return props_as_html
-    
-    def _children_to_str(self, level: int = 2) -> str:
-        if self.children is None:
-            return "None"
-        
-        tab = " " * 2 * level
-        outer_tab = " " * 2 * (level - 1)
-        child_lines: list[str] = []
-
-        opening = "[\n"
-        for child in self.children:
-            child_lines.append(tab + child._format_node_string(level + 1))
-        closing = "\n" + outer_tab + "]"
-        return opening + ",\n".join(child_lines) + closing
-    
-    def _props_to_str(self, level: int = 2) -> str:
-        if self.props is None:
-            return "None"
-        
-        tab = " " * 2 * level 
-        outer_tab = " " * 2 * (level - 1)
-        lines: list[str] = []
-
-        opening = "{\n"
-        for prop, value in self.props.items():
-            lines.append(tab + prop + ": " + value)
-        closing = "\n" + outer_tab + "}"
-        return opening + ",\n".join(lines) + closing
-    
-    def _format_node_string(self, level: int = 1) -> str:
-        if self.children is None:
-            return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
-
-        tab = " " * 2 * level 
-        outer_tab = " " * 2 * (level - 1)
-        lines: list[str] = []
-
-        opening = "HTMLNode(\n"
-        lines.append(tab + str(self.tag))
-        lines.append(tab + str(self.value))
-        lines.append(tab + self._children_to_str(level + 1))
-        lines.append(tab + self._props_to_str(level + 1)) 
-        closing = "\n" + outer_tab + ")"
-        return opening + ",\n".join(lines) + closing
 
     def __repr__(self) -> str:
-        return self._format_node_string()
+        return f"HTMLNode({self.tag}, {self.value}, children:{self.children}, {self.props})"
     
 class LeafNode(HTMLNode):
     def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None) -> None:
@@ -79,12 +35,8 @@ class LeafNode(HTMLNode):
             raise ValueError("leaf node must have a value")
         if self.tag is None:
             return self.value
-        opening = f"<{self.tag}{self.props_to_html()}>"
-        closing = f"</{self.tag}>"
-        return opening + self.value + closing
-    
-    def _format_node_string(self, level: int = 1) -> str:
-        return str(self)
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
     def __repr__(self) -> str:
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
