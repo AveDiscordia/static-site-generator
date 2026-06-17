209acfb6b7d20be93c3021b2debaeb19c561af80
AveDiscordia <guinto.eris0618@gmail.com>
2026-06-17 02:56:44 -0400

    fixed split_nodes_image returning empty list; added function to turn plain strings into textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -65,6 +65,7 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
 
         if remaining != "":
             split_nodes.append(TextNode(remaining, TextType.TEXT))
+        new_nodes.extend(split_nodes)
     return new_nodes
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
@@ -91,3 +92,11 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
             split_nodes.append(TextNode(remaining, TextType.TEXT))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    node = TextNode(text, TextType.TEXT)
+    bold = split_nodes_delimiter([node], "**", TextType.BOLD)
+    italic= split_nodes_delimiter(bold, "_", TextType.ITALIC)
+    code= split_nodes_delimiter(italic, "`", TextType.CODE)
+    image= split_nodes_image(code)
+    return split_nodes_link(image)
