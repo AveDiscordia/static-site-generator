be5b50dc8fb0b4dac5dd390cb69740b252ab5915
AveDiscordia <guinto.eris0618@gmail.com>
2026-06-17 22:39:05 -0400

    added generate_page.py; updated main.py to build webpage after copying

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from copystatic import remove_dir_contents, copy_directory_recursive
+from generate_page import generate_page
 
 static_path = "./static"
 public_path = "./public"
@@ -7,6 +8,8 @@ def main() -> None:
     remove_dir_contents(public_path)
     print()
     copy_directory_recursive(static_path, public_path)
+    print()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
