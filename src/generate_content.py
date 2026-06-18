8cffc30f717b674bf748f1c2af49f73b7c28f618
AveDiscordia <guinto.eris0618@gmail.com>
2026-06-18 00:04:38 -0400

    added recursive page generation function

---
+++ src/generate_content.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+from markdown_to_html import markdown_to_html_node
+
+def extract_title(markdown: str) -> str:
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line.removeprefix("# ").strip()
+    raise ValueError("markdown file missing h1 header")
+
+def generate_page(from_path: str, template_path: str, dest_path: str | Path):
+    print(f"Generating webpage from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as md_file:
+        markdown = md_file.read()
+    with open(template_path, "r") as temp_file:
+        template = temp_file.read()
+
+    title: str = extract_title(markdown)
+    content: str = markdown_to_html_node(markdown).to_html()
+    full_html = template.replace("{{ Title }}", title)
+    full_html = template.replace("{{ Content }}", content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+    
+    with open(dest_path, "w") as webpage:
+        webpage.write(full_html)
+
+def generate_pages_recursively(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursively(from_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
 from copystatic import remove_dir_contents, copy_directory_recursive
-from generate_page import generate_page
+from generate_content import generate_pages_recursively
 
 static_path = "./static"
 public_path = "./public"
+content_path = "./content"
+markdown_paths = [
+    "./content/index.md",
+    "./content/blog/glorfindel/index.md",
+    "./content/blog/tom/index.md",
+    "./content/blog/majesty/index.md",
+    "./content/contact/index.md",
+]
 
 def main() -> None:
+    print("Deleting public directory...")
     remove_dir_contents(public_path)
     print()
+
+    print("Copying static files to public directory...")
     copy_directory_recursive(static_path, public_path)
     print()
-    generate_page("content/index.md", "template.html", "public/index.html")
+
+    print("Generating content...")
+    generate_pages_recursively(content_path, "template.html", public_path)
+
 
 if __name__ == "__main__":
     main()
